086826ab45808d7f14a04c34a506ab7b8cae965b
Raoni Demarchi <raoni.demarchi@luizalabs.com>
2020-05-11 18:20:38 -0300

    style: add image lazy load

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
-
-import image from "../images/gatsby-astronaut.png"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi astronaut</h1>
+    <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <img src={image} alt="An astronaut" />
+      <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>

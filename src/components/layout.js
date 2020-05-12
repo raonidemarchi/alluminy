ef32f5f1a70fbb517542c65e3e38943c93f1006a
Raoni Demarchi <raoni.demarchi@luizalabs.com>
2020-05-12 12:54:07 -0300

    style: remove comments from seo component

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,16 +18,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer></footer>
-      </div>
+      <main>{children}</main>
+      <footer></footer>
     </>
   )
 }

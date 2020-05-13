eaa7e0a8789b87fcdb68d24a342da0b341f161e6
Raoni Demarchi <raoni.demarchi@luizalabs.com>
2020-05-13 18:27:18 -0300

    feat: increase fotter button size

--- src/components/ActionButtonFooter.js
+++ src/components/ActionButtonFooter.js
@@ -3,7 +3,11 @@ import ActionButton from "./ActionButton"
 
 const ActionButtonFooter = () => (
   <aside>
-    <ActionButton className={"d-md-none fixed-bottom w-100 rounded-0"}>
+    <ActionButton
+      className={"d-md-none fixed-bottom rounded-0"}
+      width="100%"
+      height="60px"
+    >
       Entre em contato
     </ActionButton>
   </aside>

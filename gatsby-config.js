b7313566feb6fe1994406f012de0fc4a22ba7980
Raoni Demarchi <raoni.demarchi@luizalabs.com>
2020-05-18 15:52:29 -0300

    build: remove prefix paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  pathPrefix: `/alluminy`,
   siteMetadata: {
     title: `Alluminy · Esquadrias de Alumínio`,
     description: `A Alluminy é uma empresa que atua há mais de 15 anos no mercado de fabricação e instalação de esquadrias de alumínio sob medida, e atende projetos dos mais variados tipos, que incluem edifícios residenciais, comerciais, industriais, além de residências de alto e médio padrão, clubes, academias, entre outros.`,

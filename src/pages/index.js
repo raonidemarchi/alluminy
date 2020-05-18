0322b3779365fc405b3cd05bb746323118ca77db
Raoni Demarchi <raoni.demarchi@luizalabs.com>
2020-05-18 19:02:42 -0300

    refactor: change project types component name

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import React from "react"
 import Layout from "../components/Layout"
 import Banner from "../components/pages/Banner"
 import WhoAreWe from "../components/pages/WhoAreWe"
-import Projects from "../components/pages/Projects"
+import ProjectTypes from "../components/pages/ProjectTypes"
 import OurAdvantages from "../components/pages/OurAdvantages"
 
 const IndexPage = () => (
   <Layout>
     <Banner />
     <WhoAreWe />
-    <Projects />
+    <ProjectTypes />
     <OurAdvantages />
   </Layout>
 )

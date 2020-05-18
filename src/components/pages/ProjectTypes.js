8e236a7346adf8f01a445f9388665d9cbf46f158
Raoni Demarchi <raonidemarchi@gmail.com>
2020-05-18 19:57:56 -0300

    style: remove unnecessary container in project types

--- src/components/pages/ProjectTypes.js
+++ src/components/pages/ProjectTypes.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import { Row, Col } from "react-bootstrap"
 import styled from "styled-components"
 
 import firstProject from "../../images/project-1.jpg"
@@ -12,9 +12,6 @@ import { devices } from "../../constants"
 
 const Section = styled.section`
   margin: 4rem 0;
-`
-
-const SectionContainer = styled(Container)`
   max-width: 1600px;
 `
 
@@ -47,106 +44,104 @@ const SectionRow = styled(Row)`
 `
 
 const WhoAreWe = () => (
-  <Section className="bg-light pb-4">
-    <SectionContainer fluid>
-      <SectionRow className="mb-5">
-        <Col md={6}>
-          <Img mt={-40} src={firstProject}></Img>
-        </Col>
-        <Col
-          md={{ span: 5, offset: 1 }}
-          lg={4}
-          xl={3}
-          className="text-center text-md-left py-4"
-        >
-          <Title>RESIDÊNCIAS</Title>
-          <p className="lead">
-            Trabalhamos na execução de residências de alto e médio padrão,
-            fabricando e instalando portas, janelas, guarda corpo, portões, etc.
-          </p>
-        </Col>
-      </SectionRow>
-      <SectionRow>
-        <Col
-          md={{ span: 5 }}
-          lg={{ span: 4, offset: 1 }}
-          xl={{ span: 3, offset: 2 }}
-          className="text-center text-md-left py-4 order-2"
-        >
-          <Title>EDIFÍCIOS RESIDENCIAIS</Title>
-          <p className="lead">
-            Temos grande experiência no mercado de edifícios residenciais,
-            fabricando e instalando esquadrias de alumínio conforme a
-            necessidade do projeto.
-          </p>
-        </Col>
-        <Col
-          md={{ span: 6, offset: 1 }}
-          xl={{ offset: 1 }}
-          className="order-md-2"
-        >
-          <Img src={secondProject}></Img>
-        </Col>
-      </SectionRow>
-      <SectionRow className="mb-5">
-        <Col md={6}>
-          <Img src={thirdProject}></Img>
-        </Col>
-        <Col
-          md={{ span: 5, offset: 1 }}
-          lg={4}
-          xl={3}
-          className="text-center text-md-left py-4"
-        >
-          <Title>EDIFÍCIOS COMERCIAIS</Title>
-          <p className="lead">
-            Além de trabalhar com as esquadrias tradicionais, somos
-            especialistas na fabricação e instalação de fachadas, tais como
-            glazing, cortina e brises.
-          </p>
-        </Col>
-      </SectionRow>
-      <SectionRow>
-        <Col
-          md={{ span: 5 }}
-          lg={{ span: 4, offset: 1 }}
-          xl={{ span: 3, offset: 2 }}
-          className="text-center text-md-left py-4 order-2"
-        >
-          <Title>INSTALAÇÕES INDUSTRIAIS</Title>
-          <p className="lead">
-            Trabalhamos com uma ampla variedade de portas, portões e janelas,
-            além de sermos especialistas na fabricação e instalação de fachadas,
-            tais como glazing, cortina e brises.
-          </p>
-        </Col>
-        <Col
-          md={{ span: 6, offset: 1 }}
-          xl={{ offset: 1 }}
-          className="order-md-2"
-        >
-          <Img src={fourthProject}></Img>
-        </Col>
-      </SectionRow>
-      <SectionRow className="mb-5">
-        <Col md={6}>
-          <Img src={fifthProject}></Img>
-        </Col>
-        <Col
-          md={{ span: 5, offset: 1 }}
-          lg={4}
-          xl={3}
-          className="text-center text-md-left py-4"
-        >
-          <Title>CLUBES E ACADEMIAS</Title>
-          <p className="lead">
-            Atendemos os mais diversos projetos de clubes e academias,
-            realizando desde o fechamento de fachadas até a fabricação e
-            instalação de divisórias sanitárias.
-          </p>
-        </Col>
-      </SectionRow>
-    </SectionContainer>
+  <Section className="bg-light pb-4 container-fluid">
+    <SectionRow className="mb-5">
+      <Col md={6}>
+        <Img mt={-40} src={firstProject}></Img>
+      </Col>
+      <Col
+        md={{ span: 5, offset: 1 }}
+        lg={4}
+        xl={3}
+        className="text-center text-md-left py-4"
+      >
+        <Title>RESIDÊNCIAS</Title>
+        <p className="lead">
+          Trabalhamos na execução de residências de alto e médio padrão,
+          fabricando e instalando portas, janelas, guarda corpo, portões, etc.
+        </p>
+      </Col>
+    </SectionRow>
+    <SectionRow>
+      <Col
+        md={{ span: 5 }}
+        lg={{ span: 4, offset: 1 }}
+        xl={{ span: 3, offset: 2 }}
+        className="text-center text-md-left py-4 order-2"
+      >
+        <Title>EDIFÍCIOS RESIDENCIAIS</Title>
+        <p className="lead">
+          Temos grande experiência no mercado de edifícios residenciais,
+          fabricando e instalando esquadrias de alumínio conforme a necessidade
+          do projeto.
+        </p>
+      </Col>
+      <Col
+        md={{ span: 6, offset: 1 }}
+        xl={{ offset: 1 }}
+        className="order-md-2"
+      >
+        <Img src={secondProject}></Img>
+      </Col>
+    </SectionRow>
+    <SectionRow className="mb-5">
+      <Col md={6}>
+        <Img src={thirdProject}></Img>
+      </Col>
+      <Col
+        md={{ span: 5, offset: 1 }}
+        lg={4}
+        xl={3}
+        className="text-center text-md-left py-4"
+      >
+        <Title>EDIFÍCIOS COMERCIAIS</Title>
+        <p className="lead">
+          Além de trabalhar com as esquadrias tradicionais, somos especialistas
+          na fabricação e instalação de fachadas, tais como glazing, cortina e
+          brises.
+        </p>
+      </Col>
+    </SectionRow>
+    <SectionRow>
+      <Col
+        md={{ span: 5 }}
+        lg={{ span: 4, offset: 1 }}
+        xl={{ span: 3, offset: 2 }}
+        className="text-center text-md-left py-4 order-2"
+      >
+        <Title>INSTALAÇÕES INDUSTRIAIS</Title>
+        <p className="lead">
+          Trabalhamos com uma ampla variedade de portas, portões e janelas, além
+          de sermos especialistas na fabricação e instalação de fachadas, tais
+          como glazing, cortina e brises.
+        </p>
+      </Col>
+      <Col
+        md={{ span: 6, offset: 1 }}
+        xl={{ offset: 1 }}
+        className="order-md-2"
+      >
+        <Img src={fourthProject}></Img>
+      </Col>
+    </SectionRow>
+    <SectionRow className="mb-5">
+      <Col md={6}>
+        <Img src={fifthProject}></Img>
+      </Col>
+      <Col
+        md={{ span: 5, offset: 1 }}
+        lg={4}
+        xl={3}
+        className="text-center text-md-left py-4"
+      >
+        <Title>CLUBES E ACADEMIAS</Title>
+        <p className="lead">
+          Atendemos os mais diversos projetos de clubes e academias, realizando
+          desde o fechamento de fachadas até a fabricação e instalação de
+          divisórias sanitárias.
+        </p>
+      </Col>
+    </SectionRow>
   </Section>
 )
 

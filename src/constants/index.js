50964abf56b89660e1e39bc10fa18c4a9f0c98f1
Raoni Demarchi <raonidemarchi@gmail.com>
2020-06-27 10:15:33 -0300

    style: change google maps location

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const alluminyPhone = "(19) 3394-3654"
 export const alluminyWhatsApp = "+55 (19) 99203-0454"
-export const alluminyGoogleMapsAddress = "https://goo.gl/maps/r1vn7NtKVJD4JZxx5"
+export const alluminyGoogleMapsAddress = "https://g.page/Alluminy?share"
 export const alluminyEmail = "jrcerigatto@uol.com.br"
 export const alluminyFacebook =
   "https://www.facebook.com/Alluminy-Esquadrias-de-Alum%C3%ADnio-1446937995598231/"

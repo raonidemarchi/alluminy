c494103cdb7f1c44aeb913cc69d64b02e6d7a37d
Raoni Demarchi <raonidemarchi@gmail.com>
2020-07-02 22:31:03 -0300

    style: remove smooth scrol from css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,6 @@
 import React from "react"
 import { ModalProvider } from "./src/components/contexts/ModalContext"
-import { createGlobalStyle } from "styled-components"
-
-const GlobalStyle = createGlobalStyle`
-  html {
-    scroll-behavior: smooth;
-  }
-`
 
 export const wrapRootElement = ({ element }) => (
-  <>
-    <ModalProvider>{element}</ModalProvider>
-    <GlobalStyle />
-  </>
+  <ModalProvider>{element}</ModalProvider>
 )

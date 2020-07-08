2827f45b25560f941b788a3f36f51d5aa198c89c
Raoni Demarchi <raonidemarchi@gmail.com>
2020-07-08 19:24:25 -0300

    style: change site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Alluminy · Esquadrias de Alumínio`,
+    title: `Alluminy - Esquadrias de Alumínio`,
     description: `A Alluminy é uma empresa que atua há mais de 15 anos no mercado de fabricação e instalação de esquadrias de alumínio sob medida, e atende projetos dos mais variados tipos, que incluem edifícios residenciais, comerciais, industriais, além de residências de alto e médio padrão, clubes, academias, entre outros.`,
     author: `@raonidemarchi`,
     siteUrl: `https://alluminy.com.br`,

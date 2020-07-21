91bc075f2fb2554351281e2f7377d0e033972ace
Raoni Demarchi <raoni.demarchi@luizalabs.com>
2020-07-21 18:58:29 -0300

    perf: reduce images sizes from project gallery on mobile

--- src/components/pages/ProjectsGallery.js
+++ src/components/pages/ProjectsGallery.js
@@ -20,6 +20,23 @@ import Project14 from "../../images/projects-gallery/project-14.webp"
 import Project15 from "../../images/projects-gallery/project-15.webp"
 import Project16 from "../../images/projects-gallery/project-16.webp"
 
+import Project1Sm from "../../images/projects-gallery/sm/project-1.webp"
+import Project2Sm from "../../images/projects-gallery/sm/project-2.webp"
+import Project3Sm from "../../images/projects-gallery/sm/project-3.webp"
+import Project4Sm from "../../images/projects-gallery/sm/project-4.webp"
+import Project5Sm from "../../images/projects-gallery/sm/project-5.webp"
+import Project6Sm from "../../images/projects-gallery/sm/project-6.webp"
+import Project7Sm from "../../images/projects-gallery/sm/project-7.webp"
+import Project8Sm from "../../images/projects-gallery/sm/project-8.webp"
+import Project9Sm from "../../images/projects-gallery/sm/project-9.webp"
+import Project10Sm from "../../images/projects-gallery/sm/project-10.webp"
+import Project11Sm from "../../images/projects-gallery/sm/project-11.webp"
+import Project12Sm from "../../images/projects-gallery/sm/project-12.webp"
+import Project13Sm from "../../images/projects-gallery/sm/project-13.webp"
+import Project14Sm from "../../images/projects-gallery/sm/project-14.webp"
+import Project15Sm from "../../images/projects-gallery/sm/project-15.webp"
+import Project16Sm from "../../images/projects-gallery/sm/project-16.webp"
+
 const Section = styled.section`
   margin-bottom: 2rem;
 
@@ -72,28 +89,29 @@ const ProjectsGallery = () => (
     <h2 className="text-center text-uppercase display-5">Nossas obras</h2>
 
     <Grid>
-      <Img src={Project1} />
-      <Img src={Project2} />
-      <Img src={Project3} />
-      <Img src={Project4} rowSpan={2} />
-      <Img src={Project5} />
-      <Img src={Project6} />
-      <Img src={Project7} />
-      <Img src={Project8} rowSpan={2} />
-      <Img src={Project9} />
-      <Img src={Project10} />
-      <Img src={Project11} />
+      <Img src={Project1} srcSm={Project1Sm} />
+      <Img src={Project2} srcSm={Project2Sm} />
+      <Img src={Project3} srcSm={Project3Sm} />
+      <Img src={Project4} srcSm={Project4Sm} rowSpan={2} />
+      <Img src={Project5} srcSm={Project5Sm} />
+      <Img src={Project6} srcSm={Project6Sm} />
+      <Img src={Project7} srcSm={Project7Sm} />
+      <Img src={Project8} srcSm={Project8Sm} rowSpan={2} />
+      <Img src={Project9} srcSm={Project9Sm} />
+      <Img src={Project10} srcSm={Project10Sm} />
+      <Img src={Project11} srcSm={Project11Sm} />
       <Img
         src={Project12}
+        srcSm={Project12Sm}
         rowSpan={2}
         colSpan={2}
         rowSpanSm={1}
         colSpanSm={1}
       />
-      <Img src={Project13} />
-      <Img src={Project14} />
-      <Img src={Project15} />
-      <Img src={Project16} />
+      <Img src={Project13} srcSm={Project13Sm} />
+      <Img src={Project14} srcSm={Project14Sm} />
+      <Img src={Project15} srcSm={Project15Sm} />
+      <Img src={Project16} srcSm={Project16Sm} />
     </Grid>
   </Section>
 )
